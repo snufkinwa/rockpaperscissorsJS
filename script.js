f091b803a2d7717616b58399be91a3b693ce5e02
Janay Harris <janlynnh.916@gmail.com>
2021-08-08 20:49:32 -0500

    Update

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function loss(userSelection, computerSelection){
 }
 function draw(userSelection, computerSelection){
     /*Informs player of a draw*/
-    result_p.innerHTML = `${convertToWord(userSelection)} is equal to ${convertToWord(computerSelection)}. It's a Tie!🪢`;    
+    result_p.innerHTML = `${convertToWord(userSelection)} is equal to ${convertToWord(computerSelection)}. It's a Tie! 🪢`;    
 }
 
 function game(userSelection){
